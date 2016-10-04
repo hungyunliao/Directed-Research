0c1a35205e7c20f7ee24448fb58347c65f730e67
Hung-Yun Liao <hungyunliao@gmail.com>
2016-10-03 20:53:56 -0700

    catch input text from text fileds

--- DirectedResearch/ViewController.swift
+++ DirectedResearch/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var input1: UITextField!
+    @IBOutlet weak var input2: UITextField!
+    @IBOutlet weak var input3: UITextField!
+    @IBOutlet weak var input4: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func submit(sender: UIButton) {
+        let input = (one: input1.text, two: input2.text, three: input3.text, four: input4.text)
+        label1.text = input.one
+        label2.text = input.two
+        label3.text = input.three
+        label4.text = input.four
     }
 
     override func didReceiveMemoryWarning() {
