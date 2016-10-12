dc4363c0049d967f4383e5fddbc32dcab5709b8c
Hung-Yun Liao <hungyunliao@gmail.com>
2016-10-12 16:00:00 -0700

    got returned JSON

---
+++ DirectedResearch/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  DirectedResearch
+//
+//  Created by Hung-Yun Liao on 10/12/16.
+//  Copyright © 2016 Hung-Yun Liao. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var textRegion: UILabel!
+    
+    var toPass:String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textRegion.text = toPass
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DirectedResearch/ViewController.swift
+++ DirectedResearch/ViewController.swift
@@ -72,11 +72,53 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         myAlert.addAction(okAction)
         myAlert.addAction(cancelAction)
         self.presentViewController(myAlert, animated: true, completion: nil)
+        
+        postRequest(input.userName!, psw: input.password!)
+        
     }
+    
+    var responseString: String = ""
+    
+    func postRequest(usrname: String, psw: String) {
+//        let request = NSMutableURLRequest(URL: NSURL(string: "http://cs-server.usc.edu:33627/HW8/test.php?StreetName=2352Portland&City=LA&State=CA&Degree=us")!)
+        let urlString = "https://tartan.plaid.com/connect?client_id=test_id&secret=test_secret&username=" + usrname + "&password=" + psw + "&type=wells"
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+        request.HTTPMethod = "POST"
+        //        let postString = "id=13&name=Jack"
+        //        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+            guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            self.responseString = String(data: data!, encoding: NSUTF8StringEncoding)!
+            print("responseString = \(self.responseString)")
+        }
+        task.resume()
+
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: (UIStoryboardSegue!), sender: AnyObject!) {
+        if (segue.identifier == "showJSON") {
+            let svc = segue!.destinationViewController as! DetailViewController;
+            
+            svc.toPass = responseString
+            
+        }
     }
 
 
